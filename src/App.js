493a155173e606da7d5209c9f62bfad29628678f
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-05-23 18:30:11 -0500

    [ADD] Base Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// External Packages
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+// Pages
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <div className="container"></div>
+        {/* <Switch>
+            <Route exact path="/" >
+            </Route>
+            <Route exact path="/login" >
+            </Route>
+            <Route exact path="/posts" >
+            </Route>
+            <Route exact path="/posts/:id" >
+            </Route>
+        </Switch> */}
+      </div>
+    </Router>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
