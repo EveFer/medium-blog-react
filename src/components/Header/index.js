23e37753bc7a835cc1dcd2ad86b5546473b07677
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-05-24 22:12:48 -0500

    [ADD] Finished Component Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,49 +3,53 @@ import React, { Component } from 'react'
 import logo from '../../assets/img/logo medium-desktop.png'
 import avatar from '../../assets/img/avatar.png'
 
+// External Packages
+import { Link } from 'react-router-dom'
+
 import './Header.scss'
 
-export default class Header extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
+function Header({isAuthenticated}) {
     return (
         <div className="row">
           <div className="col-12">
-            {/* Aqui componente Header - Fer */}
             <header className="d-flex justify-content-between">
-              <div className="p-3 ">
+              <div className="p-3">
                 <img class="logo-medium d-none d-md-block" src={logo} alt="logo" />
                 <i className="fab fa-medium d-md-none"></i>
               </div>
-              <div className="mt-2 d-flex align-items-center">
-                {/* <span className="icon-search"><i id="view" data-toggle="tooltip" title="Search Medium" className="fas fa-search mobile-distribution margin-right"></i></span>  */}
-                
-                <label className="icon-search" for="isChecked"><i id="view" data-toggle="tooltip" title="Search Medium" className="fas fa-search mobile-distribution margin-right"></i></label>
-                <input type="checkbox" id="isChecked"></input>
-                <input 
-                  id="ocultar" 
-                  type="text" 
-                  placeholder="Search Medium"
-                  className="rounded border-dark text-center border-0 btn-busqueda mr-2" 
-                />
+              {
+                isAuthenticated ? (
+                  <div className="mt-2 d-flex align-items-center">
+                    <i id="view" data-toggle="tooltip" title="Search Medium" className="fas fa-search mobile-distribution margin-right"></i>
+                    <input 
+                      id="ocultar" 
+                      type="text" 
+                      placeholder="Search Medium"
+                      className="rounded border-dark text-center border-0 btn-busqueda mr-2" 
+                    />
+                    <i data-toggle="tooltip" title="Bookmarks" className="far fa-bookmark d-none d-md-block "></i>
+                    <i data-toggle="tooltip" title="Notifications" className="far fa-bell margin-right"></i>
+                    <button 
+                      type="button"
+                      className="btn rounded upgrade-header border border-dark d-none d-md-block mr-2 ">Upgrade</button>
+                    <img class="avatar-medium margin-right " src={avatar} alt=""></img>
+                  </div>
 
-                
-                
-                
-                <i data-toggle="tooltip" title="Bookmarks" className="far fa-bookmark d-none d-md-block "></i>
-                <i data-toggle="tooltip" title="Notifications" className="far fa-bell margin-right"></i>
-                <button 
-                  type="button"
-                  className="btn rounded upgrade-header border border-dark d-none d-md-block mr-2 "
-                >Upgrade</button>
-                <img class="avatar-medium margin-right " src={avatar} alt=""></img>
-              </div>
+                ) : (
+                  <div className="mt-2 d-flex w-50 align-items-center justify-content-between wrapper-links">
+                    <Link to="/subscribe">Subscribe</Link>
+                    <Link to="/write">Write</Link>
+                    <Link to="/SignIn">Sign in</Link>
+                    <button 
+                      type="button"
+                      className="btn-get-started upgrade-header d-none d-md-block mr-2 ">Get started</button>
+                  </div>
+                )
+              }
             </header>
           </div>
         </div>
     )
   }
-}
+
+  export default Header
