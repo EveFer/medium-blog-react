da9379d14a199e52bf3150125e49036521c920f9
noe lopez avila <noerr_@hotmail.com>
2020-05-26 00:02:57 -0500

    feature-navbar

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -2,43 +2,39 @@ import React, { Component } from 'react'
 
 // Components
 import Header from '../../components/Header'
+import Navbar from '../../components/Navbar'
 
 export default class Posts extends Component {
-  render() {
+  render () {
     return (
       <>
         <Header />
 
-        <div className="row">
-          <div className="col-12">
-            {/* Aqui componente Navbar - Noe */}
-            NAVBAR
+        <Navbar />
+
+        <div className='row'>
+          <div className='col-12 col-md-6 col-lg-4'>
+            {/* Aqui componente Banner - Marce */}
+            {/* MARCE */}
+          </div>
+          <div className='col-12 col-md-6 col-lg-4'>
+            {/* Aqui componente Banner - Marce */}
+            {/* MARCE 1 */}
+          </div>
+          <div className='col-12 col-md-6 col-lg-4'>
+            {/* Aqui componente Banner - Marce */}
+            {/* MARCE 2 */}
           </div>
         </div>
 
-        <div className="row">
-          <div className="col-12 col-md-6 col-lg-4">
-            {/* Aqui componente Banner - Marce */}
-            MARCE
+        <div className='row'>
+          <div className='col-12 col-lg-8'>
+            {/* Aqui componente MiddleSection - Gerardo */}
+            {/* GERRARDO */}
           </div>
-          <div className="col-12 col-md-6 col-lg-4">
-            {/* Aqui componente Banner - Marce */}
-            MARCE 1
-          </div>
-          <div className="col-12 col-md-6 col-lg-4">
-            {/* Aqui componente Banner - Marce */}
-            MARCE 2
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-12 col-lg-8">
-              {/* Aqui componente MiddleSection - Gerardo */}
-              GERRARDO
-          </div>
-          <div className="col-12 col-lg-4">
-              {/* Aqui componente MiddleSection - Gerardo */}
-              GERARDO
+          <div className='col-12 col-lg-4'>
+            {/* Aqui componente MiddleSection - Gerardo */}
+            {/* GERARDO */}
           </div>
         </div>
       </>
