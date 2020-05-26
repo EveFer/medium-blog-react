81a5141aac3db50669634aa89dcdfa6fdbfea67f
Gerardo <gerardo@localhost.localdomain>
2020-05-26 17:43:57 -0500

    RenderPostsFromDb

--- src/components/MiddleSection/index.js
+++ src/components/MiddleSection/index.js
@@ -1,26 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect, Component } from "react";
 
 
-import Aside from '../../components/Aside'
+import Aside from '../../components/Aside';
 
-import './Cards.scss'
-import Post from './Post'
 
-function Cards(props) {
+import './Cards.scss';
+import Post from './Post';
+import {GetPosts, GetPost} from "../../services/posts";
+
+
+
+export default class Cards extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+    this._renderPosts = this._renderPosts.bind(this)
+  }
+
+  async componentDidMount(){
+    try {
+      const response = await GetPosts()
+      const dataResponse = await response.json()
+      console.log(dataResponse.data.posts)
+      this.setState({
+        posts : dataResponse.data.posts      
+      })
+    } catch (error) {
+      console.log('Error', error)
+    }
+
+  }
+  _renderPosts(){
+    const { posts } = this.state;
+    return posts.map(({_id,category,title,description,author,publication_date,estimated_time,image}) => (
+    <Post key={_id} 
+    category={category} 
+    title={title} 
+    description={description} 
+    author={author} 
+    publication_date={publication_date} 
+    estimated_time={estimated_time} 
+    image={image}
+    />
+    ))
+  }
+
+  render(){
     return (
       <div className="row">
         <div className="col-lg-8 col-md-12">
-          <Post/>
-          <Post/>
-          <Post/>
+          {this._renderPosts()}
         </div>
       <div className="col-lg-4 col-md-12">
         <Aside/>
       </div>
     </div>
-
-
     )
-}
-    
-export default Cards
+  }
+} 
